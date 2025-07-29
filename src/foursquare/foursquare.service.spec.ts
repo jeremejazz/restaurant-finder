1ac4a1f1603833ed3b6441bec3ce3a2f7ffe6366
Jereme <jeremejazz15@gmail.com>
2025-07-29 22:43:54 +0800

    update unit tests

--- src/foursquare/foursquare.service.spec.ts
+++ src/foursquare/foursquare.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FoursquareService } from './foursquare.service';
+import { HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
 
 describe('FoursquareService', () => {
   let service: FoursquareService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FoursquareService],
+      providers: [
+        FoursquareService,
+        {
+          provide: HttpService,
+          useValue: {},
+        },
+        {
+          provide: ConfigService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<FoursquareService>(FoursquareService);
--- src/gemini/gemini.service.spec.ts
+++ src/gemini/gemini.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GeminiService } from './gemini.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('GeminiService', () => {
   let service: GeminiService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GeminiService],
+      providers: [
+        GeminiService,
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<GeminiService>(GeminiService);
