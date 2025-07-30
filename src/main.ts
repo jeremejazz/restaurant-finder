9d34c21a3b3be1a42472241b13a230770e9a35a5
Jereme <jeremejazz15@gmail.com>
2025-07-30 10:56:12 +0800

    update vercel deployment

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -6,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
-  app.setGlobalPrefix('api');
+  // app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 
