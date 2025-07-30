82aa4840e965f1038d4c831903bf7ab58b7e372c
Jereme <jeremejazz15@gmail.com>
2025-07-30 13:10:59 +0800

    implement swagger

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,22 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Restaurant Finder')
+    .setDescription('Recommendeds restaurant based on your input')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
--- src/restaurant/dto/restaurant-search.dto.ts
+++ src/restaurant/dto/restaurant-search.dto.ts
@@ -1,10 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class RestaurantSearchDto {
   @IsString()
   @IsNotEmpty()
   @MaxLength(400)
+  @ApiProperty({
+    description: 'Message prompt for LLM',
+    maximum: 400,
+  })
   message: string;
 
+  @ApiProperty({
+    description: 'API Key',
+  })
   code: string;
 }
