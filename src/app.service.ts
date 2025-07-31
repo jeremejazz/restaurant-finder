3f7750cfbd0d126b211b4f57f9ed1014e1ea1c66
Jereme <jeremejazz15@gmail.com>
2025-07-31 10:42:11 +0800

    chore: update landing page

--- src/app.service.ts
+++ src/app.service.ts
@@ -10,10 +10,17 @@ export class AppService {
             <style>body{ font-family: sans-serif;}</style>
         </head>
         <body>
+        <div style="text-align:center;">
+
             <h1>Restaurant Finder</h1>
             <p>
-            For more information, see <a href="/docs">API Docs</a>
+              <img src="https://res.cloudinary.com/jereme/image/upload/v1753929555/projects/resto-finder/coffee-and-toast.jpg" alt="Coffee and Toast" height="300" />
+            </p>            
+            <p>
+            For more information, please see <a href="/docs">API Docs</a>.
             </p>
+
+          </div>
         </body>
     </html>
     `;
