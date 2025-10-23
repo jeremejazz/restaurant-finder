6383f290fe7fd81ff5bb4d5475e66f753a8015f5
Jereme Causing <jeremejazz15@gmail.com>
2025-10-24 06:39:20 +0800

    update UI

--- client/src/components/ui/search/search.tsx
+++ client/src/components/ui/search/search.tsx
@@ -1,41 +1,23 @@
 import {
   Box,
-  Card,
   Container,
-  Flex,
-  For,
   Heading,
   Input,
   InputGroup,
   Stack,
-  VStack,
+
 } from '@chakra-ui/react';
+import { VscSparkleFilled } from "react-icons/vsc";
+
 import SearchResults from './search-results';
+import SearchBox from './search-box';
 
 const Search = () => {
   return (
     <>
       <Stack align="center" my={10} gap={30}>
         <Box as="section" textAlign="center" width="100%" display="flex" justifyContent="center">
-          <Stack gap={6} width={{ base: '90%', md: '500px' }}>
-            <Heading as="h1" textAlign="center" size="2xl">
-              Search
-            </Heading>
-
-            {/* Input group with an icon */}
-            <InputGroup >
-              <Input
-          type="text"
-          placeholder="Search here..."
-          borderRadius="full"
-          boxShadow="md"
-          _focus={{
-            borderColor: 'blue.400',
-            boxShadow: 'outline',
-          }}
-              />
-            </InputGroup>
-          </Stack>
+            <SearchBox />
         </Box>
         <Container>
           <Stack gap="4" direction="row" wrap="wrap">
