a5e62f70c527f82e5d13edb9038d5f75806d4e3e
Jereme Causing <jeremejazz15@gmail.com>
2025-10-23 14:50:54 +0800

    add light switch toggle

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,11 +1,13 @@
 import { Box, Heading } from '@chakra-ui/react';
+import { ColorModeButton } from './ui/color-mode';
 
 const Header = () => {
   return (
     <Box as="header"  shadow="sm" px={4} py={3}>
-    <Heading as="h1" size="lg" color="white" textAlign="center">
+    <Heading as="h1" size="lg"   textAlign="center">
       Restaurant Finder
     </Heading>
+    <ColorModeButton position="absolute" top={2} right={4} />
     </Box>
   );
 };
